ee69683e173e80ffd50838809043b55c9b636feb
AndrejErjavec <erandrej@]mail.com>
2023-11-04 22:45:25 +0100

    setup database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+// naloži spremenljivke iz dataoteke .env v globalno spremenljivko process.env
+dotenv.config();
 
 const app = express();
-
 const port = 5000;
 
+/**
+ * HTTP REQUESTS
+ * --------------------------------------------
+ * GET - pridobiti podatke (read)
+ * POST - pošlji podatke (create)
+ * PUT - posodobi obstoječe podatke (update)
+ * DELETE - izbriši podatke (delete)
+*/
+
+app.get('/person', (req, res) => {
+  console.log(req.query);
+  const gender = req.query.gender;
+  const age = req.query.age;
+  res.send(`You requested gender ${gender} with age ${age}`);
+})
+
 app.get('/hello', (req, res) => {
-  res.send("hello")
+  res.send("hello");
 })
 
 app.get('/burek', (req, res) => {
@@ -15,4 +33,6 @@ app.get('/burek', (req, res) => {
 app.listen(port, (err, res) => {
   if (err) console.log(err);
   console.log(`Server listening on port ${port}`);
+  // beremo iz process.env
+  // console.log(process.env.WELCOME_MESSAGE)
 });
